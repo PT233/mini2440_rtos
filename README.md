9dc4bfe6bf58b167e024eff18ecd74a3b7181b1d
PT233 <1046647732@qq.com>
2025-12-01 14:35:42 +0800

    OS_TCB结构体

--- README.md
+++ README.md
@@ -1,4 +1,16 @@
 ```text
+1.因为uc/os是可裁剪的系统，通过设置os_cfg.h实现，然而我们没必要考虑这个因素。去掉所有条件执行，改为全部执行，除非影响系统稳定性，例如test.c中：
+#if OS_CRITICAL_METHOD == 3
+    OS_CPU_SR  cpu_sr;
+#endif
+改为：
+    OS_CPU_SR  cpu_sr;
+
+2.去掉所有的扩展功能函数，只保留基础功能，OSTimeDlyHMSM与OSTimeDly，但没必要去掉：
+扩展函数：OSTaskCreateExt、OSMboxPostOpt、OSQPostOpt
+基础函数：OSTaskCreate、OSMboxPost、OSQPost
+3.有歧义的函数缩写一概全称
+
 mini2440_rtos/
 ├── arch/                           # 【架构层】
 │   ├── startup.S                   # 启动代码（需要修改中断函数地址，加上export，直接b跳转到对应c语言中断函数）
@@ -21,7 +33,7 @@ mini2440_rtos/
 │   ├── os_debug.h                  # debug头文件
 │   ├── os_timer.h                  # 计时器
 │   ├── os_uart.h                   # uart
-│   └── os.h                        # os头文件
+│   └── os.h                        # os头文件，每个模块的结构体放这里
 │
 ├── kernel/                         # 【内核层】
 │   ├── os_core.c                   # 调度器
@@ -33,7 +45,7 @@ mini2440_rtos/
 │   ├── os_sem.c                    # 信号量
 │   ├── os_task.c                   # 任务管理
 │   ├── os_time.c                   # 时间管理
-│   └── os.c
+│   └── os.c			    # 
 │
 ├── user/                            # 【应用层】
 │   ├── os_debug.c                   # 实现可重入函数和print函数
--- include/os.h
+++ include/os.h
@@ -2,4 +2,30 @@
 #define __OS_H__
 
 
+/*
+*   OS_TCB Task Control Block
+*   
+*/
+typedef struct OS_TCB{
+    OS_STACK        *OSTCBStackPtr;             //指向当前任务栈的栈顶指针
+    //双向链表
+    struct OS_TCB   *OSTCBPrevPtr;              //指向上一个TCB任务控制块的指针
+    struct OS_TCB   *OSTCBNextPtr;              //指向下一个TCB任务控制块的指针
+    //OS_EVENT 还没定义
+    OS_EVENT        *OSTCBEventPtr;             //指向任务所等待的事件控制块的指针
+    void            *OSTCBMessage;              //从邮箱或者消息队列中的消息
+    //OS_FLAG_NODE 还没定义
+    OS_FLAG_NODE    *OSTCBFlagNodePtr;          //指向时间标志节点的指针
+    OS_FLAGS         OSTCBFlagsReady;           //任务就绪态的事件标志
+    INT16U           OSTCBDelay;                //延迟任务的节拍数，或者等待事件的超时
+    INT8U            OSTCBPrio;                 //任务优先级
+    INT8U            OSTCBStatus;               //任务状态
+    INT8U            OSTCB_X;                   //任务优先级的组中的位位置，在os_task.c中
+    INT8U            OSTCB_Y;                   //任务优先级在组中的就绪表索引
+    INT8U            OSTCBBitmap_X;             //访问就绪表中的位位置的位掩码
+    INT8U            OSTCBBitmap_Y;             //访问就绪表中的位位置的位掩码
+    BOOLEAN          OSTCBDeleteReq;            //任务等待事件的返回值
+}OS_TCB;
+
+
 #endif // __OS_H__
